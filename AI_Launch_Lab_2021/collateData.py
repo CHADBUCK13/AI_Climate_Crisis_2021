da27d089241fde32185fcd3e6bb33cb6a1da5171
Talib Hussain <ultiguy@hotmail.com>
2021-01-23 12:59:47 -0500

    rename to add space

--- AI_Launch_Lab_2021/collateData.py
+++ AI_Launch_Lab_2021/collateData.py
@@ -15,31 +15,33 @@ def collateData():
         year = row['YEAR']
         #df = pd.read_csv('d:/CodingProjects/' + state + '.csv')
 
-        # Verify file exists
-        df = pd.read_csv('WeatherData/' + state + '.csv', skiprows=(17))
-        print(df.head())
-        for j, row2 in df.iterrows():
-            year2 = row2['YEAR']
-            type = row2['PARAMETER']
-            if year == year2:
-                #print row2
-                collatedData = collatedData.append({'YEAR': year,
-                           'STATE': state,
-                           'HONEY': row['HONEY'],
-                           'PARAMETER': row2['PARAMETER'],
-                           'JAN': row2['JAN'],
-                           'FEB': row2['FEB'],
-                           'MAR': row2['MAR'],
-                           'APR': row2['APR'],
-                           'MAY': row2['MAY'],
-                           'JUN': row2['JUN'],
-                           'JUL': row2['JUL'],
-                           'AUG': row2['AUG'],
-                           'SEP': row2['SEP'],
-                           'OCT': row2['OCT'],
-                           'NOV': row2['NOV'],
-                           'DEC': row2['DEC'],
-                           'ANN': row2['ANN']}, ignore_index=True)
+        print(">>>>>>>>>>>>>>>State being loaded is" + state)
+        if state != 'OTHER STATES':
+            # Verify file exists
+            df = pd.read_csv('WeatherData/' + state + '.csv', skiprows=(17))
+            print(df.head())
+            for j, row2 in df.iterrows():
+                year2 = row2['YEAR']
+                type = row2['PARAMETER']
+                if year == year2:
+                    #print row2
+                    collatedData = collatedData.append({'YEAR': year,
+                               'STATE': state,
+                               'HONEY': row['HONEY'],
+                               'PARAMETER': row2['PARAMETER'],
+                               'JAN': row2['JAN'],
+                               'FEB': row2['FEB'],
+                               'MAR': row2['MAR'],
+                               'APR': row2['APR'],
+                               'MAY': row2['MAY'],
+                               'JUN': row2['JUN'],
+                               'JUL': row2['JUL'],
+                               'AUG': row2['AUG'],
+                               'SEP': row2['SEP'],
+                               'OCT': row2['OCT'],
+                               'NOV': row2['NOV'],
+                               'DEC': row2['DEC'],
+                               'ANN': row2['ANN']}, ignore_index=True)
 
     print(collatedData)
     collatedData.to_csv('d:/CodingProjects/collated.csv', index=False)
