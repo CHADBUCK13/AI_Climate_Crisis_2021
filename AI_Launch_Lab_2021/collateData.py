471664acc3742d4d318e4db0acc960f12f327291
Talib Hussain <ultiguy@hotmail.com>
2021-01-23 12:47:30 -0500

    Add Honey data and updated script

--- AI_Launch_Lab_2021/collateData.py
+++ AI_Launch_Lab_2021/collateData.py
@@ -2,39 +2,44 @@
 import pandas as pd
 
 def collateData():
-    hn = pd.read_csv('d:/CodingProjects/honey.csv')
-    print(hn.head())
+    try:
+      hn = pd.read_csv('HoneyData/Honey.csv')
+      print(hn.head())
+    except IOError:
+      print("File not accessible")
 
-    collatedData = pd.DataFrame(columns=['YEAR', 'STATE', 'HONEY', 'JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC', 'ANN'])
+    collatedData = pd.DataFrame(columns=['PARAMETER', 'YEAR', 'STATE', 'HONEY', 'JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC', 'ANN'])
 
-    match="WS2M"
     for i, row in hn.iterrows():
         state = row['STATE']
         year = row['YEAR']
-        if state == 'ALABAMA':
-            df = pd.read_csv('d:/CodingProjects/' + state + '.csv')
-            print(df.head())
-            for j, row2 in df.iterrows():
-                year2 = row2['YEAR']
-                type = row2['PARAMETER']
-                if year == year2 and type == match:
-                    print row2
-                    collatedData = collatedData.append({'YEAR': year,
-                               'STATE': state,
-                               'HONEY': row['HONEY'],
-                               'JAN': row2['JAN'],
-                               'FEB': row2['FEB'],
-                               'MAR': row2['MAR'],
-                               'APR': row2['APR'],
-                               'MAY': row2['MAY'],
-                               'JUN': row2['JUN'],
-                               'JUL': row2['JUL'],
-                               'AUG': row2['AUG'],
-                               'SEP': row2['SEP'],
-                               'OCT': row2['OCT'],
-                               'NOV': row2['NOV'],
-                               'DEC': row2['DEC'],
-                               'ANN': row2['ANN']}, ignore_index=True)
+        #df = pd.read_csv('d:/CodingProjects/' + state + '.csv')
+
+        # Verify file exists
+        df = pd.read_csv('WeatherData/' + state + '.csv', skiprows=(17))
+        print(df.head())
+        for j, row2 in df.iterrows():
+            year2 = row2['YEAR']
+            type = row2['PARAMETER']
+            if year == year2:
+                #print row2
+                collatedData = collatedData.append({'YEAR': year,
+                           'STATE': state,
+                           'HONEY': row['HONEY'],
+                           'PARAMETER': row2['PARAMETER'],
+                           'JAN': row2['JAN'],
+                           'FEB': row2['FEB'],
+                           'MAR': row2['MAR'],
+                           'APR': row2['APR'],
+                           'MAY': row2['MAY'],
+                           'JUN': row2['JUN'],
+                           'JUL': row2['JUL'],
+                           'AUG': row2['AUG'],
+                           'SEP': row2['SEP'],
+                           'OCT': row2['OCT'],
+                           'NOV': row2['NOV'],
+                           'DEC': row2['DEC'],
+                           'ANN': row2['ANN']}, ignore_index=True)
 
     print(collatedData)
     collatedData.to_csv('d:/CodingProjects/collated.csv', index=False)
